ed03e0e09de1698d69646896b03b761bd2ef7316
dshaw0004 <dipankarshaw692+github@gmail.com>
2022-10-08 21:13:54 +0530

    favicon and background improvement

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const textbox = document.getElementById("input");
 const btn2 = document.getElementById("btn2");
 function log() {
 	let input = textbox.value;
+	// img.classList.remove("invisible");
 	if (input === "") {
 		alert("Please fill the input");
 		return;
 	}
 	img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`;
 	img.classList.add("img-border");
-	// btn2.style.visibility = "visible";
+	// btn2.classList.remove("invisible");
 }
 function clear() {
-	img.src = " ";
-	textbox.value = " ";
-	btn2.style.visibility = "hidden";
+	img.classList.add("invisible");
+	btn2.classList.add("invisible");
 }
